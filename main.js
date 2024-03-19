9749ea6859ad688c9b93497d8ad26c50950c77cd
Suelengon <135610228+Suelengon@users.noreply.github.com>
2024-03-19 12:23:01 -0300

    parte 6

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querrSelectorAll(".aba-conteudo")
 
 for(lesi=0;i <botoes.length;i++){
    botoes[i].onclik = function(){
 
    for(let j=0;j<botoes.length;j++){
         botoes[j].classList.remove("ativo");
+        botoes[j].classList.remove("ativo");
    }
 
     botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo")
    }
 }
 
